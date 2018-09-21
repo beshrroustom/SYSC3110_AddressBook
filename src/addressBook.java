8698aaa8db3d9105b43ad515a836008eecd3e324
beshrroustom <beshrroustom@mc6045-21.labs.sce.carleton.ca>
2018-09-21 08:57:05 -0400

    xcc

---
+++ src/addressBook.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class addressBook {
+	private ArrayList<buddyInfo> list;
+	
+	public addressBook() {
+		this.list= new ArrayList<buddyInfo>();
+	}
+	public boolean addBuddy(buddyInfo data) {
+		
+		this.list.add(data);
+		
+		return true;
+	}
+	
+	public boolean removeBuddy(buddyInfo data) {
+		
+		this.list.remove(data);
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello ");
+		buddyInfo friend2=new buddyInfo("Beshr");
+		addressBook book1= new addressBook();
+		book1.addBuddy(friend2);
+		book1.removeBuddy(friend2);
+	}
+}
---
+++ src/buddyInfo.java
@@ -0,0 +1,27 @@
+
+public class buddyInfo {
+private int age;
+private String name;
+
+public buddyInfo(String name) {
+	this.name=name;
+	this.age=18;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+}
